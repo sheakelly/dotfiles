99f79692eed052ebce5b6bf91cf863f9cd21744d
Shea Kelly <shea.kelly@lwb.org.au>
2020-01-17 08:55:16 +1100

    start of brew files

---
+++ brew.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# tap some casks
+brew tap caskroom/cask
+brew tap homebrew/cask-fonts
+
+# Install dev apps and tools
+brew install git
+brew install ruby
+brew install tmux
+brew install neovim
+brew install zsh
+brew install ripgrep
+brew install fzf
+brew install z
+brew cask install font-hack-nerd-font
+# nvm and node
+# pyenv and python
+
+# Install other apps
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install todoist
+brew cask install spotify
+brew cask install slack
+brew install p7zip
+
