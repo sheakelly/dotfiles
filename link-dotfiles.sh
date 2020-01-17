d2204017b310425729a0c28a2a052b49654880c5
Shea Kelly <shea.kelly@lwb.org.au>
2020-01-17 11:19:41 +1100

    renaming things

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -6,9 +6,9 @@ BACKUP_DIR=$INSTALL_DIR/backup
 echo "Backing up exising file to $BACKUP_DIR"
 
 mkdir -p $BACKUP_DIR
-mv $HOME/.zshrc $BACKUP_DIR
-mv $HOME/.tmux.conf $BACKUP_DIR
-mv $HOME/.config/nvim $BACKUP_DIR
+mv $HOME/.zshrc $BACKUP_DIR/.zshrc 2>/dev/null
+mv $HOME/.tmux.conf $BACKUP_DIR 2>/dev/null
+mv $HOME/.config/nvim $BACKUP_DIR 2>/dev/null
 
 ln -s $INSTALL_DIR/.zshrc $HOME/.zshrc
 ln -s $INSTALL_DIR/.tmux.config $HOME/.tmux.config
